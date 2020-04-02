cabf53d16256def8f959e149f91dd782ab3b482d
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-04-02 02:09:53 +0200

    first commit
    not working yet

---
+++ src/server/index.tsx
@@ -0,0 +1,12 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+io.on('connection', (socket: any) => {
+    console.log(`SocketId ${socket.id}`)
+});
+
+http.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
